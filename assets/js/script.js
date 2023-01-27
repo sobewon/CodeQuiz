b214bc6b80d6aed1be68f48ecb3f2d66fa00c95c
John Magnuson <sobewon@gmail.com>
2023-01-27 15:35:27 -0500

    fixed timer, it is now working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,58 +1,60 @@
 
+var timeEl = document.getElementById("time");
+const startGame = document.getElementById("startGame");
 
-
-var secondsLeft = 60;
-
+const tempTime = 5;
+var secondsLeft = tempTime;
 // Timer function for  countdown after clicking "Start Game" button
 function timer() {
   var timerInterval = setInterval(function() {
     secondsLeft--;
-    timeEl.textContent = secondsLeft + " seconds left till colorsplosion.";
-
-    if(secondsLeft === 0) {
+    timeEl.textContent = "GAME OVER in " + secondsLeft + " seconds.";
+ 
+    if(secondsLeft === -1) {
       // Stops execution of action at set interval
       clearInterval(timerInterval);
       // Calls function to create and append image
-      sendMessage();
+      alert("GAME OVER!");
+      secondsLeft = tempTime;
+
+      outOfTime()
     }
 
   }, 1000);
 }
 
-
-
-
-function saveScore() {
-  // Save related form data as an object
-  var score = {
-    win: win.value,
-    loss: loss.value
-};
-  // Use .setItem() to store object in storage and JSON.stringify to convert it as a string
-  localStorage.setItem("score", JSON.stringify(score));
+function outOfTime() {
+  timeEl.textContent = " ";
 }
 
-function renderScore() {
-  // Use JSON.parse() to convert text to JavaScript object
-  var saveScore = JSON.parse(localStorage.getItem("savedScore"));
-    //var saveScore = wordbank[1];
+var question = document.getElementById("question-container");
 
-  // Check if data is returned, if not exit out of the function
-  if (saveScore !== null) {
-  document.getElementById("wins").innerHTML = saveScore.win;
-  document.getElementById("loss").innerHTML = saveScore.loss;
-} else {
-    return;
-  }
+function showQuestion() {
+  
 }
 
-var passwordText = document.querySelector("#guessWord");
 
-passwordText.value = word;
 
-// The init() function fires when the page is loaded 
-function init() {
-  // When the init function is executed, the code inside renderScore function will also execute
-  renderScore();
-}
-init();
+
+
+
+
+
+startGame.addEventListener("click", timer)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
